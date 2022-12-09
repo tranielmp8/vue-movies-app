49c07d5a15de28a516c5969d371df4eef7d5f1b6
Traniel Pride <tranielmp8@gmail.com>
2022-12-08 21:56:26 -0600

    updating title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <router-link to="/" >
-      <h1><span>Vue</span>Movies</h1>
+      <h1>Kakashi's<span>Vue</span>Movies App</h1>
     </router-link>
   </header>
   <main>
